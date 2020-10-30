341f27e1fd038f49850fd9a5a47499b1ddb8be8f
monica_ursu0608@yahoo.com <Adrian1958!>
2020-10-30 12:03:38 +0200

    tema10partea2

---
+++ public/tema10partea2/tema10partea2.js
@@ -0,0 +1,8 @@
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest()
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const colors = request.response;
+}
